287e6517512a2683d7193a444649b13e0b38fb8c
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-12 11:39:07 +0300

    ADD: db connection

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 
-const {tasksRouter} = require("./routes/api");
-const {authRouter} = require("./routes/api");
+const {tasksRouter, authRouter} = require("./routes/api");
 
 const app = express();
 
 const formatLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
 app.use(logger(formatLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use('/api/users', authRouter);
-app.use('/api/tasks', tasksRouter);
+app.use('/users', authRouter);
+app.use('/tasks', tasksRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: "Not found" });
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require('./app');
+// const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { DB_HOST, PORT } = process.env;
+
+
+console.log(DB_HOST);
+console.log(PORT);
+
+
+
 
 app.listen(3000, () => {
-    console.log("Server running. Use our API on port: 3000");
+    console.log(`Server running. Use our API on port: ${PORT}`);
 });
