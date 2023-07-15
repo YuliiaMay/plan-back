8cd752a3a0b83727d7eef18e58969de633c6c904
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-15 20:39:41 +0300

    FIX: db connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
+require('dotenv').config();
+
 
 const {tasksRouter, authRouter} = require("./routes/api");
 
