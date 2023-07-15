28df73d98c3e6e48f5ec0b97add23f74b5ccf836
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-07-15 19:10:24 +0300

    FIX

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,6 @@ const { validateBody } = require('../../middlewares');
 const { userSchemas } = require('../../models');
 const { register, login } = require('../../controllers/auth');
 
-
 const router = express.Router();
 
 router.post('/register', validateBody(userSchemas.registerSchema), register);
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const { DB_HOST, PORT } = process.env;
 
-
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
